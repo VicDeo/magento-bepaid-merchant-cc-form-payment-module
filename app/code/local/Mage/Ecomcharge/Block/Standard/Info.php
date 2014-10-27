aed8eac646e51ce17ef43b56b9ff9c8bece6f8d9
VicDeo <victor.dubiniuk@gmail.com>
2014-10-27 12:58:24 +0300

    Show transaction Id in payment info

--- app/code/local/Mage/Ecomcharge/Block/Standard/Info.php
+++ app/code/local/Mage/Ecomcharge/Block/Standard/Info.php
@@ -1,11 +1,11 @@
 <?php
 class Mage_Ecomcharge_Block_Standard_Info extends Mage_Payment_Block_Info
 {
-	protected function _construct()
+    protected function _construct()
     {
         parent::_construct();     
     }
- 	
+
     protected function _prepareSpecificInformation($transport = null)
     {
         if (null !== $this->_paymentSpecificInformation) {
@@ -14,22 +14,11 @@ class Mage_Ecomcharge_Block_Standard_Info extends Mage_Payment_Block_Info
         $info = $this->getInfo();
         $transport = new Varien_Object();
         $transport = parent::_prepareSpecificInformation($transport);
-		if ($this->gettr_no())
-        $transport->addData(array(
-            Mage::helper('payment')->__('Transaction ID') => $this->gettr_no(),
-    //        Mage::helper('payment')->__('Card No') => $this->getInfo()->getCcNumberEnc(),			
-        ));
+        if ($this->getInfo()->getLastTransId()){
+           $transport->addData(array(
+                Mage::helper('payment')->__('Transaction ID') => $this->getInfo()->getLastTransId(),
+            ));
+        }
         return $transport;
     }
-	
-	public function gettr_no()
-	{    
-	    $tr_no = '';
-		$details = @unserialize($this->getInfo()->getAdditionalData());
-		$tr_no = $details['Transaction_Id'];
-
-		return $tr_no; 
-		
-      }
-
 }
