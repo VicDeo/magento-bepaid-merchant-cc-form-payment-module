7228c5f2459d1597aad92f0e22c97062c91e7c0c
avsh <avterus@gmail.com>
2014-11-04 12:02:05 +0300

    cosmetic updates

--- app/code/local/Mage/Ecomcharge/Model/Source/ModeAction.php
+++ app/code/local/Mage/Ecomcharge/Model/Source/ModeAction.php
@@ -6,10 +6,7 @@ class Mage_Ecomcharge_Model_Source_ModeAction
     {
         return array(
             array('value' => Mage_Ecomcharge_Model_Config::MODE_LIVE, 'label' => Mage::helper('ecomcharge')->__('Live')),
-            array('value' => Mage_Ecomcharge_Model_Config::MODE_TEST, 'label' => Mage::helper('ecomcharge')->__('Test')),			
+            array('value' => Mage_Ecomcharge_Model_Config::MODE_TEST, 'label' => Mage::helper('ecomcharge')->__('Test')),
         );
     }
 }
-
-
-
