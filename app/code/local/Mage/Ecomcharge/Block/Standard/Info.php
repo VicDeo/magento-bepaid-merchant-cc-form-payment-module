3465f24fd459b71e65f50d64aeeae9b1bc131a01
avsh <avterus@gmail.com>
2014-11-21 14:33:16 +0300

    Show transaction Id in payment info

--- app/code/local/Mage/Ecomcharge/Block/Standard/Info.php
+++ app/code/local/Mage/Ecomcharge/Block/Standard/Info.php
@@ -2,34 +2,23 @@
 class Mage_Ecomcharge_Block_Standard_Info extends Mage_Payment_Block_Info
 {
 	protected function _construct()
-    {
-        parent::_construct();     
-    }
- 	
-    protected function _prepareSpecificInformation($transport = null)
-    {
-        if (null !== $this->_paymentSpecificInformation) {
-            return $this->_paymentSpecificInformation;
-        }
-        $info = $this->getInfo();
-        $transport = new Varien_Object();
-        $transport = parent::_prepareSpecificInformation($transport);
-		if ($this->gettr_no())
-        $transport->addData(array(
-            Mage::helper('payment')->__('Transaction ID') => $this->gettr_no(),
-    //        Mage::helper('payment')->__('Card No') => $this->getInfo()->getCcNumberEnc(),			
-        ));
-        return $transport;
-    }
-	
-	public function gettr_no()
-	{    
-	    $tr_no = '';
-		$details = @unserialize($this->getInfo()->getAdditionalData());
-		$tr_no = $details['Transaction_Id'];
+  {
+    parent::_construct();
+  }
 
-		return $tr_no; 
-		
+  protected function _prepareSpecificInformation($transport = null)
+  {
+      if (null !== $this->_paymentSpecificInformation) {
+          return $this->_paymentSpecificInformation;
       }
-
+      $info = $this->getInfo();
+      $transport = new Varien_Object();
+      $transport = parent::_prepareSpecificInformation($transport);
+      if ($this->getInfo()->getLastTransId()) {
+          $transport->addData(array(
+            Mage::helper('payment')->__('Transaction ID') => $this->getInfo()->getLastTransId(),
+            ));
+      }
+      return $transport;
+  }
 }
