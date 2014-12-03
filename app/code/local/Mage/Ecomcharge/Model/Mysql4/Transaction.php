6ceae33bc08c9eceb85dd790b6f2432c3dea2849
VicDeo <victor.dubiniuk@gmail.com>
2014-12-03 14:40:58 +0300

    Use primary key on model init

--- app/code/local/Mage/Ecomcharge/Model/Mysql4/Transaction.php
+++ app/code/local/Mage/Ecomcharge/Model/Mysql4/Transaction.php
@@ -2,7 +2,7 @@
 
 class Mage_Ecomcharge_Model_Mysql4_Transaction extends Mage_Core_Model_Mysql4_Abstract{
     public function _construct(){
-        $this->_init('ecomcharge/transaction');
+        $this->_init('ecomcharge/transaction', 'id_ecomcharge_transaction');
     }
 }
 
